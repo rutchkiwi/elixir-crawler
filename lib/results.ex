426fc8397b5ba0402d6a58c95c0040f91f7a02ce
Viktor Holmberg <viktor.holmberg@gmail.com>
2015-11-03 21:33:14 +0000

    wip on results

---
+++ lib/results.ex
@@ -0,0 +1,32 @@
+defmodule Results do
+	
+	def start_link() do
+		{:ok, pid} = Task.start_link(&_wait/0)
+		Process.register(pid, __MODULE__)
+	end
+
+	def report_visited_uri(uri) do
+		send(__MODULE__, {:visited, URI.to_string(uri)})
+	end
+
+	def get_all_results() do
+		send(__MODULE__, {:give_results, self()})
+		receive do
+			{:give_results_answer, all_results} -> all_results
+		end
+	end
+
+	def _wait() do
+		{caller, all_results} = receive do
+			{:give_results, caller} -> {caller, _fetch_all_results(HashSet.new)}
+		end
+		send(caller, {:give_results_answer, all_results})
+	end
+
+	def _fetch_all_results(results) do
+		receive do
+			{:visited, url} -> _fetch_all_results(Set.put(results, url))
+			after 0 -> results
+		end
+	end
+end
--- test/results_test.exs
+++ test/results_test.exs
@@ -13,8 +13,20 @@ defmodule ResultsTest do
 
   test "single" do
     Results.report_visited_uri(URI.parse("www.test.com"))
+   
     expected = HashSet.new
     expected = Set.put(expected, "www.test.com")
     assert Results.get_all_results() == expected
   end
+
+  test "multiple" do
+    Results.report_visited_uri(URI.parse("www.test1.com"))
+    Results.report_visited_uri(URI.parse("www.test2.com"))
+   
+    expected = HashSet.new
+    expected = Set.put(expected, "www.test1.com")
+    expected = Set.put(expected, "www.test2.com")
+    assert Results.get_all_results() == expected
+  end
+
 end
